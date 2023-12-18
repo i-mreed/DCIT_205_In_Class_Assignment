9351f54016356dc91565eccb82106576d26e019e
Iddrisu M. Reed <iddrisumreed@gmail.com>
2023-12-18 09:26:55 +0000

    Update patientController.js

--- ugmc-backend/controllers/patientController.js
+++ ugmc-backend/controllers/patientController.js
@@ -1,9 +1,8 @@
 const Patient = require('/models/patient');
 
-// Function to register a new patient
+
 exports.registerPatient = async (req, res) => {
-  // Implementation to save patient details to MongoDB
-  // ...
+ 
 
   res.json({ message: 'Patient registered successfully' });
 };
